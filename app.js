64bdce55923c19f96c6c5ca66c2985df849d9482
Ruslan <markov.exceedteam@gmail.com>
2022-03-30 14:08:32 +0300

    app.js & routes

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+const router = require("./routes/routes");
+
+app.use(cors());
+
+app.use(express.json());
+app.use("/api", router);
+
+app.listen(8080, () => {
+  console.log("Example app listening on port 8080!");
+});
